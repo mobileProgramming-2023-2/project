8d5e8c7792498b5cabe49ae0715b6dd8c47523f0
suurimm <suurimm45@gmail.com>
2023-11-09 15:05:44 +0900

    add 폴더 추가

---
+++ app/src/main/java/smu/mp/project/alarm/add/AlarmAddFragment.java
@@ -0,0 +1,16 @@
+package smu.mp.project.alarm.add;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import smu.mp.project.R;
+
+public class AlarmAddFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_alarm_add);
+    }
+}
