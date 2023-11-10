9a0899292b82204c3d9d61457b5c99d49177da9e
hanseul <gkstmf578@sookmyung.ac.kr>
2023-11-10 18:14:09 +0900

    feat: 할 일 리스트에 체크박스 기능 구현

--- app/src/main/java/smu/mp/project/todo/TodoAdapter.java
+++ app/src/main/java/smu/mp/project/todo/TodoAdapter.java
@@ -1,10 +1,14 @@
 package smu.mp.project.todo;
 
 import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import java.util.List;
@@ -27,9 +31,12 @@ public class TodoAdapter extends ArrayAdapter<TodoItem> {
 
         TextView contentTextView = convertView.findViewById(R.id.content);  // 할 일 내용
         TextView memoTextView = convertView.findViewById(R.id.memo);  // 할 일 메모
-        // 할 일 시간
-        TextView timeTextView = convertView.findViewById(R.id.time);
+        TextView timeTextView = convertView.findViewById(R.id.time);  // 할 일 시간
+        CheckBox checkBox = convertView.findViewById(R.id.checkBox);
+        
         TodoItem currentItem = getItem(position);
+
+        // 할 일 시간 표시 형식 설정
         String TimeRange;
         if (currentItem.getStartTime() == null && currentItem.getEndTime() == null) {
             TimeRange = "";  // 시간 모두 선택 안 되어 있으면 빈 문자열로 표시
@@ -38,10 +45,24 @@ public class TodoAdapter extends ArrayAdapter<TodoItem> {
             TimeRange = currentItem.getStartTime() + " - " + currentItem.getEndTime();
         }
 
-        // 각 TextView에 설정
+        // 데이터를 각 TextView에 설정
         contentTextView.setText(currentItem.getContent());
         memoTextView.setText(currentItem.getMemo());
         timeTextView.setText(TimeRange);
+
+        // 체크박스 Listener
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    contentTextView.setPaintFlags(contentTextView.getPaintFlags() | Paint.STRIKE_THRU_TEXT_FLAG);
+                    contentTextView.setTextColor(Color.LTGRAY);
+                } else {
+                    contentTextView.setPaintFlags(contentTextView.getPaintFlags() & (~Paint.STRIKE_THRU_TEXT_FLAG));
+                    contentTextView.setTextColor(Color.BLACK);
+                }
+            }
+        });
 
         return convertView;
     }
