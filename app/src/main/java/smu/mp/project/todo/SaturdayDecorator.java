d2217f265467056d6ccba2a3a190bce806dbc0d4
hanseul <gkstmf578@sookmyung.ac.kr>
2023-11-11 22:46:07 +0900

    feat: 캘린더 커스텀 - 주말 색상 오류 수정, 오늘 날짜 및 선택된 날짜 디자인, 투두타이틀 설정

--- app/src/main/java/smu/mp/project/todo/SaturdayDecorator.java
+++ app/src/main/java/smu/mp/project/todo/SaturdayDecorator.java
@@ -10,22 +10,27 @@ import com.prolificinteractive.materialcalendarview.DayViewFacade;
 import java.util.Calendar;
 
 
-// 캘린더 토요일을 파란색으로 적용
+// 캘린더 토요일을 파란색으로 표시
 public class SaturdayDecorator implements DayViewDecorator {
     private final Calendar calendar = Calendar.getInstance();
+    private int currentMonth = -1;  // 현재 보고 있는 달력 페이지의 월
+
+    // 현재 페이지의 월을 설정하는 메소드
+    public void setCurrentMonth(int currentMonth) {
+        this.currentMonth = currentMonth;
+    }
 
     @Override
     public boolean shouldDecorate(CalendarDay day) {
         day.copyTo(calendar);
         int weekDay = calendar.get(Calendar.DAY_OF_WEEK);
-        int currentMonth = CalendarDay.today().getMonth();
 
-        // 현재 달의 토요일인지 확인
+        // 현재 페이지의 월과 같은 달의 토요일인지 확인
         return weekDay == Calendar.SATURDAY && calendar.get(Calendar.MONTH) == currentMonth;
     }
 
     @Override
     public void decorate(DayViewFacade view) {
-        view.addSpan(new ForegroundColorSpan(Color.BLUE));
+        view.addSpan(new ForegroundColorSpan(Color.BLUE)); // 파란색 적용
     }
 }
